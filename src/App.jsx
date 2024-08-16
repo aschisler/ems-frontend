298edbcc304d01d508617a50311131a37fd3b8a2
Adina Schisler <aschisler@users.noreply.github.com>
2024-08-16 05:58:31 -0500

    React + Vite + axios for ems

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
+
+function App() {
+
+  return (
+    <>
+        <ListEmployeeComponent/>
+    </>
+  )
+}
+
+export default App
